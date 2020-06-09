8ad01816ff62e969ee96826e9d147c397c9c8e11
GreaterBeat <anthony_99@mail.ru>
2020-06-09 14:38:05 +0300

    123

--- na3.js
+++ na3.js
@@ -3,12 +3,13 @@ function test(str)
     return str.replace(/[^\d]*[+*/()-.]*[^\d]*[^\d\+\*\/\-\.\(\)]/g, '')
 }
 
-let str = '3.5 земле-копа +4 поросенка *10 рублей -- 5.5 $ /5 человек =';
+let str = '3.5 зем+л.е-копа -4 поросенка *10 рублей - 5.5 $ /5 человек =';
 let n = test(str)
 
 const applyMath = getMathHandler();
-console.log(n);
+
 console.log('Задана строка', str);
+console.log('Строка, "очищенная" от слов', n);
 console.log('Результат счёта без eval', applyMath(n));
 console.log('Результат счёта c eval', eval(n));
 
